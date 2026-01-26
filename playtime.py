acc318cc926ea63b4bd97c868dd41b8babf9a551
Thomas Steen Rasmussen <thomas@gibfest.dk>
2026-01-26 20:00:16 +0100

    support different languages

--- playtime.py
+++ playtime.py
@@ -21,11 +21,24 @@ import shutil
 import sys
 from importlib.metadata import PackageNotFoundError, version
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, TypeAlias
 
 import django_stubs_ext
 import PTN  # type: ignore[import-untyped]
 from cinemagoerng import web as imdb
+from cinemagoerng.model import (
+    Movie,
+    MusicVideo,
+    ShortMovie,
+    TVEpisode,
+    TVMiniSeries,
+    TVMovie,
+    TVSeries,
+    TVShortMovie,
+    TVSpecial,
+    VideoGame,
+    VideoMovie,
+)
 from django.db import models
 from django.utils import timezone
 from enrich.logging import RichHandler
@@ -45,6 +58,20 @@ if TYPE_CHECKING:
 logger = logging.getLogger("playtime")
 
 django_stubs_ext.monkeypatch()
+
+CgngObj: TypeAlias = (
+    Movie
+    | TVMovie
+    | ShortMovie
+    | TVShortMovie
+    | VideoMovie
+    | MusicVideo
+    | VideoGame
+    | TVSeries
+    | TVMiniSeries
+    | TVEpisode
+    | TVSpecial
+)
 
 # get version number from package metadata if possible
 try:
@@ -95,6 +122,9 @@ class CGNGData(models.Model):
         "django_imdb.Title", on_delete=models.PROTECT, related_name="cgngdata"
     )
     data: models.JSONField = models.JSONField(help_text="The movie data from CGNG")
+    language: models.CharField[str, str] = models.CharField(
+        default="en", help_text="The language code used when fetching this data."
+    )
     updated_at: models.DateTimeField[datetime.datetime, datetime.datetime] = models.DateTimeField(
         auto_now=True, help_text="Date and time when this data was last updated."
     )
@@ -170,7 +200,13 @@ class Playtime:
         return Title.objects.get(title_id=results[0])
 
     def identify_directories(
-        self, *, directories: list[Path], persist: bool, force: bool, ignore_textfiles: bool
+        self,
+        *,
+        directories: list[Path],
+        persist: bool,
+        force: bool,
+        ignore_textfiles: bool,
+        accept_languages: list[str],
     ) -> None:
         """Identify titles in directories."""
         for relpath in directories:
@@ -195,10 +231,11 @@ class Playtime:
             if persist:
                 directory.write_imdb_url()
 
-            # get/update extra metadata?
-            cgng = self.cgng_online_imdb_lookup(title=title)
-            if cgng is None:
-                continue
+            if not hasattr(title, "cgngdata"):
+                # get extra metadata in english
+                cgng = self.get_cgng_data(title=title, accept_languages=accept_languages)
+                if cgng is None:
+                    continue
 
             # download cover?
             coversrc = self.get_coverpath(title)
@@ -280,14 +317,15 @@ class Playtime:
         for directory in Directory.objects.all().order_by("path"):
             logger.info(directory)
 
-    def update_extra_metadata(self) -> None:
+    def update_extra_metadata(self, accept_languages: list[str]) -> None:
         """Get or update metadata for all Directory titles."""
         for title in Title.objects.filter(directories__isnull=False).distinct():
-            self.cgng_online_imdb_lookup(title=title)
+            self.get_cgng_data(title=title, accept_languages=accept_languages)
 
-    def cgng_online_imdb_lookup(
+    def get_cgng_data(
         self,
         title: Title,
+        accept_languages: list[str],
         keys: tuple[str, ...] = ("bottom_ranking", "top_ranking", "country_codes", "language_codes", "primary_image"),
         max_age_seconds: int = 86400 * 14,
     ) -> CGNGData | None:
@@ -295,26 +333,53 @@ class Playtime:
         # check for existing data
         try:
             existing = CGNGData.objects.get(title_id=title.title_id)
-            if (timezone.now() - existing.updated_at).seconds < max_age_seconds:
+            if (
+                existing.language in accept_languages
+                and (timezone.now() - existing.updated_at).seconds < max_age_seconds
+            ):
+                # no need to get new data
                 return existing
         except CGNGData.DoesNotExist:
             pass
 
-        # get new data
-        try:
-            data = imdb.get_title(title.title_id, accept_language="en")
-        except Exception:
-            logger.exception(f":cross_mark: Unable to lookup title {title} using cinemagoerng, skipping")
+        # do initial request in english
+        language = "en"
+        data = self.cgng_lookup(title=title, language=language)
+        if data is None:
             return None
+
+        # get non-english metadata?
+        most_spoken = data.language_codes[0]
+        if most_spoken != "en" and accept_languages and most_spoken in accept_languages:
+            # the most-spoken language in this movie is not-english,
+            # but among languages acceptable to the user, get metadata in this language
+            i18ndata = self.cgng_lookup(title=title, language=most_spoken)
+            if i18ndata is not None:
+                data = i18ndata
+                language = most_spoken
 
         # save in database
         modeldata = {k: v for k, v in data.__dict__.items() if k in keys}
-        cgng, created = CGNGData.objects.update_or_create(title_id=title.title_id, defaults={"data": modeldata})
+        cgng, created = CGNGData.objects.update_or_create(
+            title_id=title.title_id, defaults={"data": modeldata, language: language}
+        )
         if created:
-            logger.debug(f"Downloaded extra metadata for title {title}")
+            logger.debug(f"Downloaded extra metadata for title {title} in language {language}")
         else:
-            logger.debug(f"Updated extra metadata for title {title}")
+            logger.debug(f"Updated extra metadata for title {title} in language {language}")
         return cgng
+
+    def cgng_lookup(
+        self,
+        title: Title,
+        language: str,
+    ) -> CgngObj | None:
+        """Do online IMDB lookup and return the data."""
+        try:
+            return imdb.get_title(title.title_id, accept_language=language)
+        except Exception:
+            logger.exception(f":cross_mark: Unable to lookup title {title} using cinemagoerng, skipping")
+            return None
 
     #### SYMLINKS #####
 
@@ -343,15 +408,19 @@ class Playtime:
             things = [str(title.runtime_minutes // 30)] if title.runtime_minutes else []
         elif category == "rating":
             things = [title.rating.rating] if hasattr(title, "rating") else []
-        elif category == "top_ranking":
-            things = [title.cgngdata.data["top_ranking"]] if hasattr(title, "cgngdata") else []
-        elif category == "bottom_ranking":
-            things = [title.cgngdata.data["bottom_ranking"]] if hasattr(title, "cgngdata") else []
+        elif category in ["top_ranking", "bottom_ranking"]:
+            things = [title.cgngdata.data[category]] if hasattr(title, "cgngdata") else []
+        elif category == "language":
+            things = title.cgngdata.data["language_codes"] if hasattr(title, "cgngdata") else []
+        elif category == "languages":
+            things = [",".join(title.cgngdata.data["language_codes"])] if hasattr(title, "cgngdata") else []
         else:
             things = []
         return things
 
-    def create_symlink_dirs(self, *, symlink_dir: Path, categories: list[str], relative: bool = True) -> None:
+    def create_symlink_dirs(
+        self, *, symlink_dir: Path, categories: list[str], accept_languages: list[str], relative: bool = True
+    ) -> None:
         """Create symlink dirs for the requested categories."""
         symlink_dir = symlink_dir.resolve()
         if not symlink_dir.exists():
@@ -361,16 +430,20 @@ class Playtime:
         for category in categories:
             categorydir = symlink_dir / category
             self.clean_category_dir(category_dir=categorydir)
-            logger.info(f"Creating symlinks by {category} in {categorydir}...")
+            logger.info(f"Creating symlinks by {category} in {categorydir} with languages {accept_languages}...")
             for title in Title.objects.filter(directories__isnull=False):
                 logger.debug(f"Creating {category} symlinks for {title}")
                 # loop over things in this category for this title
                 for thing in self.get_title_categories(title=title, category=category):
-                    if thing is None:
+                    if thing is None or thing == "":
                         continue
                     # thing is Western, Drama, actor name, year and such
                     subdir = self.get_category_subdir(
-                        categorydir=categorydir, category=category, thing=thing, title=title
+                        categorydir=categorydir,
+                        category=category,
+                        thing=thing,
+                        title=title,
+                        accept_languages=accept_languages,
                     )
                     logger.debug(f"Creating {category} symlinks for {thing} in subdir {subdir}")
                     subdir.mkdir(exist_ok=True, parents=True)
@@ -408,29 +481,34 @@ class Playtime:
             shutil.copy(coversource, coverpath)
         coverdest = cover_dest_dir / "poster.jpg"
         # make symlink destination relative
-        coverpath = os.path.relpath(coverpath, coverdest.parent)
+        relcoverpath = os.path.relpath(coverpath, coverdest.parent)
         if not coverdest.exists():
-            logger.debug(f"Creating cover symlink for {title} from {coverdest} to {coverpath}")
-            coverdest.symlink_to(coverpath)
+            logger.debug(f"Creating cover symlink for {title} from {coverdest} to {relcoverpath}")
+            coverdest.symlink_to(relcoverpath)
 
-    def get_category_subdir(self, categorydir: Path, category: str, thing: str, title: Title) -> Path:
-        """Return the subdir to use for this category."""
+    def get_category_subdir(
+        self, categorydir: Path, category: str, thing: str, title: Title, accept_languages: list[str]
+    ) -> Path:
+        """Return the subdir to use for this title in this category."""
         subdir: Path
+        aka = self.get_title_aka(title=title, accept_languages=accept_languages)
+        aka = aka.replace(os.sep, "_")
+        dirname = f"{aka} ({title.premiered})"
         if category in ["actors", "directors", "producers", "composers", "writers"]:
             # make a level with the first letter
-            subdir = categorydir / str(thing)[0] / str(thing) / title.dirname
+            subdir = categorydir / str(thing)[0] / str(thing) / dirname
         elif category == "runtime":
             step = 30
             runtime = int(thing) * step
-            subdir = categorydir / f"{runtime}-{runtime + step}" / title.dirname
+            subdir = categorydir / f"{runtime}-{runtime + step}" / dirname
         elif category == "rating":
-            subdir = categorydir / str(thing) / f"{title.dirname} ({title.rating.votes} votes)"  # type: ignore[attr-defined]
+            subdir = categorydir / str(thing) / f"{dirname} ({title.rating.votes} votes)"  # type: ignore[attr-defined]
         elif category in ["top_ranking", "bottom_ranking"]:
             subdir = (
-                categorydir / f"{thing:04}. {title.dirname} ({title.rating.rating} with {title.rating.votes} votes)"  # type: ignore[attr-defined]
+                categorydir / f"{thing:04}. {dirname} ({title.rating.rating} with {title.rating.votes} votes)"  # type: ignore[attr-defined]
             )
         else:
-            subdir = categorydir / str(thing) / title.dirname
+            subdir = categorydir / str(thing) / dirname
         return subdir
 
     def get_coverpath(self, title: Title) -> Path:
@@ -470,6 +548,15 @@ class Playtime:
             mins = 0
             secs = seconds
         return mins, secs
+
+    def get_title_aka(self, title: Title, accept_languages: list[str]) -> str:
+        """Return the best aka for a title considering accept_languages."""
+        # use original title if cgngdata["language_codes"] containes one of the acceptable_languages
+        if title.cgngdata and "language_codes" in title.cgngdata.data and title.cgngdata.data["language_codes"]:  # type: ignore[attr-defined]
+            for lang in accept_languages:
+                if lang in title.cgngdata.data["language_codes"]:  # type: ignore[attr-defined]
+                    return title.original_title
+        return title.primary_title
 
 
 ############## BOILERPLATE #########################################################################
@@ -565,7 +652,14 @@ def get_parser() -> argparse.ArgumentParser:
 
     ###########################################
     # playtime extrameta
-    subparsers.add_parser("extrameta", help="Get/update extra metadata (cover+ranking) from IMDB.")
+    extrameta_parser = subparsers.add_parser("extrameta", help="Get/update extra metadata (cover+ranking) from IMDB.")
+    extrameta_parser.add_argument(
+        "-l",
+        "--languages",
+        type=parse_comma_str_to_list,
+        default=[],
+        help="Comma-separated list of acceptable non-english languages.",
+    )
 
     ###########################################
     # playtime symlink
@@ -592,8 +686,17 @@ def get_parser() -> argparse.ArgumentParser:
             "rating",
             "top_ranking",
             "bottom_ranking",
+            "language",
+            "languages",
         ],
         help="Movie categories to enable for 'playtime symlink'.",
+    )
+    symlink_parser.add_argument(
+        "-l",
+        "--languages",
+        type=parse_comma_str_to_list,
+        default=[],
+        help="Comma-separated list of acceptable non-english languages.",
     )
 
     ###########################################
@@ -660,6 +763,13 @@ def get_parser() -> argparse.ArgumentParser:
             "the existing files are older than this number of seconds."
         ),
     )
+    import_parser.add_argument(
+        "-l",
+        "--languages",
+        type=parse_comma_str_to_list,
+        default=[],
+        help="Comma-separated list of acceptable non-english languages.",
+    )
 
     ###########################################
     # playtime export
@@ -707,6 +817,11 @@ def get_parser() -> argparse.ArgumentParser:
         help="Do not export title.ratings.tsv.gz.",
     )
     return parser
+
+
+def parse_comma_str_to_list(values: str) -> list[str]:
+    """Parse comma-separated string to list."""
+    return values.split(",")
 
 
 def parse_args(
@@ -781,15 +896,19 @@ def main(mockargs: list[str] | None = None) -> None:
         )
 
     elif args.subparser_name == "extrameta":
-        pt.update_extra_metadata()
+        pt.update_extra_metadata(accept_languages=args.languages)
 
     elif args.subparser_name == "identify":
         pt.identify_directories(
-            directories=args.titledirs, force=args.force, ignore_textfiles=args.ignore_textfiles, persist=args.persist
+            directories=args.titledirs,
+            force=args.force,
+            ignore_textfiles=args.ignore_textfiles,
+            persist=args.persist,
+            accept_languages=args.languages,
         )
 
     elif args.subparser_name == "symlink":
-        pt.create_symlink_dirs(symlink_dir=args.symlinkdir, categories=args.categories)
+        pt.create_symlink_dirs(symlink_dir=args.symlinkdir, categories=args.categories, accept_languages=args.languages)
 
     elif args.subparser_name == "ls":
         pt.list_directories()
